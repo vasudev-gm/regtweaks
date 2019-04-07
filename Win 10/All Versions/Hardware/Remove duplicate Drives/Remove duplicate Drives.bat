3312874570508ec9c9364951d0239ca9c962ca05
CHEF-KOCH <CHEF-KOCH@protonmail.com>
2019-04-07 10:29:24 +0200

    Remove duplicate Drives
    PowerShell, batch or via registry

---
+++ Win 10/All Versions/Hardware/Remove duplicate Drives/Remove duplicate Drives.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set old=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\DelegateFolders\{F5FB2C77-0E2F-4A16-A381-3E560C68BC83}
+set new=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\DelegateFolders\{F5FB2C77-0E2F-4A16-A381-3E560C68BC83}.bak
+
+set old_wow=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\DelegateFolders\{F5FB2C77-0E2F-4A16-A381-3E560C68BC83}
+set new_wow=HKLM\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\DelegateFolders\{F5FB2C77-0E2F-4A16-A381-3E560C68BC83}.bak
+
+reg.exe copy %old% %new% /s /f
+reg.exe delete %old% /f
+
+reg.exe copy %old_wow% %new_wow% /s /f
+reg.exe delete %old_wow% /f
+

16b0d0e3d70d9dda5d2fc2e49bdd6654e63b795d
CHEF-KOCH <CHEF-KOCH@protonmail.com>
2019-04-20 15:31:32 +0200

    Windows Home Edition GPO Installer

---
+++ Win 10/All Versions/Windows 10 Home/Group Policy Editor for Windows 10 Home/GPO Installer.bat
@@ -0,0 +1,39 @@
+@echo off
+
+echo Checking for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+echo Permission check result: %errorlevel%
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+echo Requesting administrative privileges...
+goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+echo Running created temporary "%temp%\getadmin.vbs"
+timeout /T 2
+"%temp%\getadmin.vbs"
+exit /B
+
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0" 
+
+echo Batch was successfully started with admin privileges
+echo .
+cls
+Title Windows Home GPO Installer
+
+pushd "%~dp0"
+
+dir /b %SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientExtensions-Package~3*.mum >List.txt 
+dir /b %SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientTools-Package~3*.mum >>List.txt 
+
+for /f %%i in ('findstr /i . List.txt 2^>nul') do dism /online /norestart /add-package:"%SystemRoot%\servicing\Packages\%%i" 
+pause
